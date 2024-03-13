6b95b273ac70d0be65c14552d11815df29e22794
CameronK24 <cskelly15@gmail.com>
2024-03-13 11:28:35 -0600

    forgot to commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './home.css';
+import Lists from './Lists/Lists';
+
+const Home = () => {
+
+
+  return(
+    <section className="home-wrapper">
+      <div className="list-component">
+        <Lists/>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/Components/Home/Lists/Lists.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './lists.css';
+
+const Lists = () => {
+
+
+  return(
+    <section className="list-wrapper">
+      <section className="list">
+        <h2>Cam's List</h2>
+        <ul>
+          <li>lkjflkjfakljfdsafdsaf</li>
+          <li>lkjflkjfakldj</li>
+          <li>lkjflkjfakldsafewj</li>
+          <li>lkjflkjfakdfalj</li>
+          <li>lkjflkjfakdfadsafdflj</li>
+          <li>lkjflkjfaklj</li>
+        </ul>
+      </section>
+    </section>
+  );
+}
+
+export default Lists;

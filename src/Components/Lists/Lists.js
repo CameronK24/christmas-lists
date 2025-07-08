438d2a2e624e36c82d480278997609a2683a9dc4
CameronK24 <cskelly15@gmail.com>
2025-07-08 02:55:46 -0600

    keep forgetting to commit

--- src/Components/Lists/Lists.js
+++ src/Components/Lists/Lists.js
@@ -7,7 +7,7 @@ const Lists = () => {
   return(
     <section className="list-wrapper">
       <section className="list">
-        <h2>Cam's List</h2>
+        <h2>Kam's List</h2>
         <ul>
           <li>lkjflkjfakljfdsafdsaf</li>
           <li>lkjflkjfakldj</li>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,9 +5,7 @@ const Nav = () => {
   return (
     <div className='nav-wrap'>
       <ul className='nav-list'> 
-        <li>
-          <h1>HOME</h1>
-        </li>
+        <li>HOME</li>
         <li>LISTS</li>
         <li>CREATE LIST</li>
       </ul>
